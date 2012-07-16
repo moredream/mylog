24dea1a27a63ab0034399c9a98be88d536cb8162
moredream <moredream@moredream-virtual-machine.(none)>
2012-07-16 18:06:26 +0900

    Initail Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mylog::Application.config.secret_token = '412c70268265f4d83b67f66c0a1c46cbd298896d99d12136b9be60f252702087fbeada1647c572dc9f0fbbdb9eca2a5bdc10bbca491d58479a96d375b0834cfb'
